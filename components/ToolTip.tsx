49720819a33ded032d7f81fe1b4de9c9e737ef77
Denzey Mae Acosta <acosta.denzeymae032@gmail.com>
2026-07-10 16:42:23 +0800

    Revert slight tooltip

--- components/ToolTip.tsx
+++ components/ToolTip.tsx
@@ -67,7 +67,7 @@ export default function Tooltip({
           transform: [{ translateY }],
           backgroundColor:
             colorScheme === "dark"
-              ? "#2D3348"
+              ? "#1F2937"
               : "#FFFFFF",
           borderColor: colors.border,
         },
@@ -77,7 +77,7 @@ export default function Tooltip({
         style={[
           styles.accent,
           {
-            backgroundColor: colors.tint2,
+            backgroundColor: colors.tint,
           },
         ]}
       />
